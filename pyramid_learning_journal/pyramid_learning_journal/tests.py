c2a448cfc036e18114aff76b4e502a67762fcd52
Carlos Cadena <cs.cadena@gmail.com>
2017-05-31 13:33:26 -0700

    tests passing tox

--- pyramid_learning_journal/pyramid_learning_journal/tests.py
+++ pyramid_learning_journal/pyramid_learning_journal/tests.py
@@ -1,5 +1,6 @@
 from pyramid import testing
 import os
+import io
 from pyramid.response import Response
 import pytest
 
@@ -10,6 +11,7 @@ HERE = os.path.dirname(__file__)
 def httprequest():
     req = testing.DummyRequest()
     return req
+
 
 def test_return_of_views_are_responses():
     """Test if the return of views are responses."""
@@ -28,7 +30,7 @@ def test_return_of_views_are_responses():
 def test_html_content_in_response(httprequest):
     """Test the html content."""
     from pyramid_learning_journal.views.default import list_view
-    file_content = open(os.path.join(HERE, 'templates/index.html')).read()
+    file_content = io.open(os.path.join(HERE, 'templates/index.html')).read()
     response = list_view(httprequest)
     assert file_content == response.text
 
