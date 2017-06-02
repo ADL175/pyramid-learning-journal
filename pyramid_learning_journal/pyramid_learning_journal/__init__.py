447d5e7d994881e612f1fae00fdc76e211542687
ADL175 <armydavidlim@gmail.com>
2017-06-01 21:58:01 -0700

    working on DB deploy to heroku

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -4,6 +4,7 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    SETTINGS['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
