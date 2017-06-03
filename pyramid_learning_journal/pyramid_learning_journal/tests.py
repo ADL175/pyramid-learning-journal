ba52ba2585404f4ece9a01ea90c7fdd9e67c38aa
ADL175 <armydavidlim@gmail.com>
2017-06-02 18:16:07 -0700

    working on second test

--- pyramid_learning_journal/pyramid_learning_journal/tests.py
+++ pyramid_learning_journal/pyramid_learning_journal/tests.py
@@ -1,50 +1,10 @@
-from pyramid import testing
-from pyramid.httpexceptions import HTTPNotFound
 import pytest
+from pyramid import testing
+import transaction
+from pyramid.httpexceptions import HTTPNotFound
 import os
 import io
 from pyramid_learning_journal.models.meta import Base
-# from pyramid_learning_journal.views.default import JOURNALS
-# from pyramid import testing
-# from pyramid.response import Response
-# import pytest
-
-
-
-# HERE = os.path.dirname(__file__)
-#
-#
-# @pytest.fixture
-# def httprequest():
-#     req = testing.DummyRequest()
-#     return req
-
-
-# def test_return_of_views_are_responses():
-#     """Test if the return of views are responses."""
-#     from pyramid_learning_journal.views.default import (
-#         home_view,
-#         detail_view
-#     )
-#     assert isinstance(list_view(httprequest), Response)
-#     assert isinstance(detail_view(httprequest), Response)
-#     assert isinstance(create_view(httprequest), Response)
-#     assert isinstance(update_view(httprequest), Response)
-
-
-# def test_html_content_in_response(httprequest):
-#     """Test the html content."""
-#     from pyramid_learning_journal.views.default import list_view
-#     file_content = io.open(os.path.join(HERE, 'templates/index.html')).read()
-#     response = list_view(httprequest)
-#     assert file_content == response.textd
-
-
-# def check_if_ok_status_with_request(httprequest):
-#     """Check if 200 status on request."""
-#     from pyramid_learning_journal.views.default import list_view
-#     response = list_view(httprequest)
-#     assert response.status_code == 200
 
 # =================== FUNCTIONAL TESTS =============
 
@@ -86,19 +46,16 @@ def test_home_route_returns_home_content(testapp):
     """Test the thome route returns home content."""
     response = testapp.get('/')
     html = response.html
-    # import pdb; pdb.set_trace()
     assert 'Journal' in str(html.find('h1').text)
     assert 'Journal' in str(html.find('title').text)
 
-#
-# def test_home_route_listing_has_all_journals(testapp):
-#     """Test the home route listing has all journals."""
-#     response = testapp.get('/')
-#     html = response.html
-#     assert len(JOURNALS) == len(html.find_all('li'))
-#
-#
-# def test_detail_route_with_bad_id(testapp):
-#     """Test the detail route with a bad ID."""
-#     response = testapp.get('/journal/400', status=404)
-#     assert "Alchemy scaffold" in response.text
+
+def test_detail_route_returns_proper_content(testapp):
+    """Test the detail route returns proper journal entry."""
+    response = testapp.get('/journal/1')
+    html = response.html
+    import pdb; pdb.set_trace()
+    # assert response ==
+
+
+    # config.add_route('detail', '/journal/{id:\d+}')
